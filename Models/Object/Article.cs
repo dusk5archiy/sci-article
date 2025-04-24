1d7825d1e86762b0d3218443bc21bbb0b3e42884
dusk5archiy <dusqdn.eml@gmail.com>
2025-04-24 10:21:36 +0700

    create models

---
+++ Models/Object/Article.cs
@@ -0,0 +1,17 @@
+class ArticleStatus
+{
+    public const string Pending = "Pending";
+    public const string Approved = "Approved";
+    public const string Rejected = "Rejected";
+}
+class Article
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public int AuthorId { get; set; }
+    public DateOnly TimeStamp { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    public string Abstract { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public string Topic { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+}
